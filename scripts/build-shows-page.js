a33f6f3538b8afad9680e45740a65458e7267ee6
Mustafa ASGHARI <mustafaasghari@Mustafas-MacBook-Air.local>
2023-10-14 19:11:15 -0700

    add api

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,37 +1,9 @@
-const showSection = document.getElementById("show-section");
+import { BandSiteAPI } from "./band-site-api.js";
+const bandSiteAPI = new BandSiteAPI("0323f365-7506-4a0f-bddf-3ad2107eb92f");
 
-const comments = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+bandSiteAPI.getShows().then((response) => {});
+
+const showSection = document.getElementById("show-section");
 
 function createStaticComments() {
   for (let dataElement of comments) {
@@ -94,58 +66,24 @@ function createCommentElement(dateParam, venueParam, locationParam) {
   return row;
 }
 
-function createStaticCommentsForTablet() {
-  const table = document.createElement("table");
-
-  const header = document.createElement("th");
-
-  const headerCell1 = document.createElement("td");
-  headerCell1.innerHTML = "DATE";
-  header.appendChild(headerCell1);
-
-  const headerCell2 = document.createElement("td");
-  headerCell2.innerHTML = "VENUE";
-  header.appendChild(headerCell2);
-
-  const headerCell3 = document.createElement("td");
-  headerCell3.innerHTML = "LOCATION";
-  header.appendChild(headerCell3);
-
-  table.appendChild(header);
-
-  for (let dataElement of comments) {
-    const row = createCommentElement(
-      dataElement.date,
-      dataElement.venue,
-      dataElement.location
-    );
-    showSection.appendChild(row);
-  }
-}
-
-function createCommentElementForTablet(dateParam, venueParam, locationParam) {
-  const row = document.createElement("tr");
-  const cell1 = document.createElement("td");
-  row.appendChild(cell1);
-  cell1.innerHTML = dateParam;
-  cell1.classList.add("show__table-cell--date");
-
-  const cell2 = document.createElement("td");
-  row.appendChild(cell2);
-  cell2.innerHTML = venueParam;
-  cell2.classList.add("show__table-cell");
-
-  const cell3 = document.createElement("td");
-  row.appendChild(cell3);
-  cell3.innerHTML = locationParam;
-  cell3.classList.add("show__table-cell");
-
-  const button = document.createElement("button");
-  button.classList.add("show__table-button");
-  button.innerHTML = "BUY TICKETS";
-  row.appendChild(button);
-
-  return row;
-}
-
-createStaticComments();
+const Show = (show) => {
+  return `
+    <div class="show__element" >
+      <div class="show__container" >
+        <div class="show__date"> 
+          <div class="show__element-label">DATE</div>
+          <div class="show__element-value--date">${new Date(show.date)}</div>
+        </div>
+        <div class="show__venue">
+          <div class="show__element-label">VENUE</div>
+          <div class="show__element-value">${show.place}</div> 
+        </div>
+        <div class="show__location">
+          <div class="show__element-label">LOCATION</div>
+          <div class="show__element-value">${show.location}</div> 
+        </div>
+      </div>
+      <button class="show__button">BUY TICKETS</button>
+    </div>
+  `;
+};
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,7 +1,14 @@
+import { BandSiteAPI } from "./band-site-api.js";
+const bandSiteAPI = new BandSiteAPI("0323f365-7506-4a0f-bddf-3ad2107eb92f");
+
+bandSiteAPI.getComments().then((response) => {
+  createStaticComments(response);
+});
+
 const form = document.getElementById("comment-form");
 const commentSection = document.getElementById("comment-section");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const name = form.elements[0];
   const comment = form.elements[1];
@@ -9,16 +16,23 @@ form.addEventListener("submit", (event) => {
   let isNameValid = isValid(name);
   let isCommentValid = isValid(comment);
   if (isNameValid && isCommentValid) {
-    const row = createCommentElement(
-      name.value,
-      new Date().toLocaleDateString(),
-      comment.value
-    );
-    commentSection.insertBefore(row, commentSection.firstChild);
+    await bandSiteAPI.postComment({
+      name: name.value,
+      comment: comment.value,
+    });
     name.value = "";
     comment.value = "";
+
+    await rerender();
   }
 });
+
+const rerender = async () => {
+  commentSection.innerHTML = "";
+
+  const updatedComments = await bandSiteAPI.getComments();
+  createStaticComments(updatedComments);
+};
 
 function isValid(input) {
   if (input.value.trim() === "") {
@@ -28,64 +42,61 @@ function isValid(input) {
   return true;
 }
 
-const comments = [
-  {
-    photoUrl: "",
-    name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    photoUrl: "",
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    photoUrl: "",
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
-
-function createStaticComments() {
+function createStaticComments(comments) {
+  const elements = [];
   for (let dataElement of comments) {
-    const row = createCommentElement(
-      dataElement.name,
-      dataElement.date,
-      dataElement.comment
-    );
-    commentSection.appendChild(row);
+    elements.push(Comment(dataElement));
+  }
+  commentSection.innerHTML = elements;
+  addEventListeners(comments);
+}
+
+async function onDelete(event) {
+  const id = event.target.id.split("_")[0];
+  bandSiteAPI.deleteComment(id).then(() => {
+    rerender();
+  });
+}
+
+function addEventListeners(comments) {
+  for (let comment of comments) {
+    document
+      .getElementById(`${comment.id}_delete`)
+      .addEventListener("click", onDelete);
+    document
+      .getElementById(`${comment.id}_like`)
+      .addEventListener("click", onLike);
   }
 }
-function createCommentElement(nameParam, dateParam, commentParam) {
-  const row = document.createElement("div");
-  row.classList.add("comment-element");
-  const img = document.createElement("div");
-  img.classList.add("comment-photo");
-  const content = document.createElement("div");
-  const header = document.createElement("div");
-  header.classList.add("comment-header");
-  const name = document.createElement("div");
-  name.classList.add("comment-name");
-  name.innerHTML = nameParam;
-  const date = document.createElement("div");
-  date.classList.add("comment-date");
-  date.innerHTML = dateParam;
-  const paragraph = document.createElement("p");
-  paragraph.classList.add("comment-paragraph");
-  paragraph.innerHTML = commentParam;
-  row.appendChild(img);
-  row.appendChild(content);
-  content.appendChild(header);
-  header.appendChild(name);
-  header.appendChild(date);
-  content.appendChild(paragraph);
 
-  return row;
+async function onLike(event) {
+  const id = event.target.id.split("_")[0];
+  bandSiteAPI.likeComment(id).then(() => {
+    rerender();
+  });
 }
-createStaticComments();
+
+const Comment = (comment) => {
+  return `
+    <div class="comment-element" id="${comment.id}">
+      <div class="comment-photo"> </div>
+      <div>
+        <div class="comment-header">
+          <div class="comment-name">
+            ${comment.name}
+          </div>
+          <div class="comment-date">
+            ${new Date(comment.timestamp).toLocaleDateString()}
+          </div>
+        </div>
+
+        <p class="comment-paragraph"> 
+          ${comment.comment}
+        </p>
+      </div>
+      <div id="${comment.id}_likeCount">${comment.likes}</div>
+      <button id="${comment.id}_delete">DELETE</button>
+      <button id="${comment.id}_like">LIKE</button>
+    </div>
+  `;
+};
